ca60deea6c6371203fc3562a67e7bbd4c7c6fcd6
Jrodolfosilva <100287751+Jrodolfosilva@users.noreply.github.com>
2022-06-07 02:20:58 -0300

    update main_menuMobile
    Criação do conteúdo principal e do menu mobile. ajustes gerais nos espaçamentos e ux

--- project/assets/js/index.js
+++ project/assets/js/index.js
@@ -1,3 +1,63 @@
-const features = document.querySelector('.item_menu.features')
+const features = document.querySelector('.item_menu.features,.item_menu_features');
+const featuresBox = document.querySelector('.conteudo.features');
 
-features.addEventListener('click', ()=>alert())
+const company = document.querySelector('.item_menu.company,.item_menu_company')
+const companyBox = document.querySelector('.conteudo.company');
+
+const setaFeatures = document.querySelector('.seta_features')
+const setaCompany = document.querySelector('.seta_company');
+
+const menuBurguer = document.querySelector('.menu_burger');
+const menuMobile = document.querySelector('.box_mobile')
+const closeBurguer = document.querySelector('.close_mobile');
+
+
+const main = document.querySelector('body')
+
+features.addEventListener('click', ()=>{
+    featuresBox.classList.toggle('ativo');
+    companyBox.classList.remove('ativo');
+    
+    setaFeatures.classList.toggle('rote');
+    setaCompany.classList.remove('rote');
+    
+})
+
+company.addEventListener('click', ()=>{
+    companyBox.classList.toggle('ativo');
+    featuresBox.classList.remove('ativo');
+    
+    setaCompany.classList.toggle('rote');
+    setaFeatures.classList.remove('rote');
+
+})
+menuBurguer.addEventListener('click',()=>{
+    menuMobile.classList.add('ativo')
+    main.classList.add('blur')
+})
+
+closeBurguer.addEventListener('click',()=>{
+    menuMobile.classList.remove('ativo')
+    main.classList.remove('blur')
+})
+
+
+
+function mobileCompany(){
+    const companyFly = document.querySelector('.conteudo_company_fly')
+    companyFly.classList.toggle('ativo')
+    
+    const setaFeatures = document.querySelector('.seta_features_fly')
+    const setaCompany = document.querySelector('.seta_company_fly')
+    setaCompany.classList.toggle('rote');
+   
+}
+function mobileFeatures(){
+    const featuresyFly = document.querySelector('.conteudo_features_fly')
+    featuresyFly.classList.toggle('ativo')
+
+    const setaFeatures = document.querySelector('.seta_features_fly')
+    const setaCompany = document.querySelector('.seta_company_fly')
+    setaFeatures.classList.toggle('rote');
+    
+}
